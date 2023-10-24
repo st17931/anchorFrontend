b2871e1fb4fdff8e5126888cc1582d15acc78eee
Satyam Tripathi <st17931@gmail.com>
2023-10-24 10:22:12 +0530

    Fixed the rank

--- src/Components/ResultTableData.jsx
+++ src/Components/ResultTableData.jsx
@@ -1,7 +1,7 @@
 export default function ResultTableData(prop){
     return(
         <tr>
-            <td>{prop.rank+1}</td>
+            <td>{prop.rank+2}</td>
             <td>{prop.p.title}</td>
             <td className="tableData"><img src={prop.p.thumbnail}/></td>
             <td>{prop.p.views}</td>
